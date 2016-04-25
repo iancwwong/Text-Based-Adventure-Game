0e0475ac81caeb687c5402c8dda58261a15075c0
Ian Wong <wong.iancw@gmail.com>
2016-04-26 02:22:03 +1000

    Agent able to read and submit commands to game engine

--- src/Agent.py
+++ src/Agent.py
@@ -10,53 +10,97 @@
 # -------------------------------------------------------
 
 import socket
-import select
 from sys import argv
 
 # ---------------------------
-# FUNCTIONS
+# AGENT
 # ---------------------------
 
-# Obtain the data for the agent view
-# by reading in data stream, and parsing
-# into individual characters
-def readView():
-	viewI = []
-	viewStr = []
-	for i in range(0,5):
-		viewJ = []
-		for j in range(0,5):
-			if (i != 2 or j != 2):
-				viewJ.append(agentsocket.recv(1))
-			else:
-				viewJ.append('')	# agent
-		viewI.append(viewJ)
-	return viewI
+class Agent(object):
+	
+	def __init__(self, portnum):
+		# Create the TCP socket
+		self.agentsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Format and print out the view (with some formatting)
-# where view is a 5x5 list of lists
-# (each list containing one character)
-def displayView(view):
-	viewStr = "+-----+\n"
-	for i in range(0,len(view)):
-		viewStr += "|"
-		for j in range(0,len(view[i])):
-			if (i == 2 and j == 2):
-				viewStr += '^'	# agent always facing upwards on the agent's screen
-			else:
-				viewStr += view[i][j]
-		viewStr += "|\n"
-	viewStr += "+-----+\n"
-	print viewStr
+		# Connect to the game engine via localhost
+		try:
+			self.agentsocket.connect(('Localhost', portnum))
+		except socket.error, e:
+			print "Error connecting to server: %s" % e
+			exit()
 
-# Use select module to read from socket
-def selectRecv(bufferSize):
-	listen_sockets = [agentsocket]
-	read_sockets, write_sockets, error_sockets = select.select(listen_sockets, [], [])
-	for rs in read_sockets:
-		if (rs == agentsocket):
-			data = agentsocket.recv(bufferSize)
-			return data
+		self.conn_alive = True		# connection established with game engine
+
+		# Prepare the view
+		self.view = []
+
+	# Run the main procedure of the agent
+	def run(self):
+		while (self.conn_alive):
+			try:
+				# Obtain the view
+				self.readView()
+		
+				# Display the view
+				self.displayView()
+			
+				# Read input
+				userInput = raw_input('Enter Action(s): ')
+
+				# Send the input
+				self.processInput(userInput)
+
+				print ""	# formatting
+
+			except socket.error:
+
+				# Connection terminated. Exit
+				self.agentsocket.close()
+				self.conn_alive = False
+
+	# Obtain the data for the agent view
+	# by reading in data stream, and parsing
+	# into individual characters
+	def readView(self):
+		viewI = []
+		viewStr = []
+		for i in range(0,5):
+			viewJ = []
+			for j in range(0,5):
+				if (i != 2 or j != 2):
+					ch = self.agentsocket.recv(1)
+					if (ch == ''):
+						# Connection terminated
+						self.agentsocket.close()
+						self.conn_alive = False
+						exit()
+					viewJ.append(ch)
+				else:
+					viewJ.append('^')	# agent
+			viewI.append(viewJ)
+		self.view = viewI
+
+	# Format and print out the view (with some formatting)
+	# where view is a 5x5 list of lists
+	# (each list containing one character)
+	def displayView(self):
+		viewStr = "+-----+\n"
+		for i in range(0,len(self.view)):
+			viewStr += "|"
+			for j in range(0,len(self.view[i])):
+				viewStr += self.view[i][j]
+			viewStr += "|\n"
+		viewStr += "+-----+"
+		print viewStr
+
+	# Sends the user input to game engine
+	def processInput(self, userInput):
+		# Send out the message
+		try:
+			self.agentsocket.send(userInput)
+		except socket.error, e:
+			print "Error sending: %s" % e
+			self.conn_alive = False
 
 # ---------------------------
 # MAIN
@@ -71,20 +115,9 @@ if (len(argv) < 3):
 script, portFlag, portnumString = argv
 portnum = int(portnumString)			# Where game engine is listening
 
-# Create the TCP socket
-agentsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to the game engine via localhost
-print "Connecting to game engine..."
-try:
-	agentsocket.connect(('Localhost', portnum))
-except socket.error, e:
-	print "Error connecting to server: %s" % e
-	exit()
-
-# Obtain and print the view
-displayView(readView())
+# Create and run the agent
+agent = Agent(portnum)
+agent.run()
 
 # Exit
-agentsocket.close()
 exit()
