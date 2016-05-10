a1cfd1a24ffaa15bbc5044e7fbae0dd6d35a7db4
Ian Wong <wong.iancw@gmail.com>
2016-05-10 18:58:26 +1000

    Started basic implementation of decision maker

---
+++ src/DecisionMaker.py
@@ -0,0 +1,29 @@
+# This is the class that represents the decision making component of the agent
+
+#! /usr/bin/python
+
+class DecisionMaker(object):
+	
+	# Constants
+	ACTION_FORWARD = 'f'
+	ACTION_RIGHT = 'r'
+	ACTION_LEFT = 'l'
+	ACTION_CHOP = 'c'
+	ACTION_UNLOCK = 'u'
+
+	# Attributes
+	past_actions		# a list of past actions (consisting of single chars)
+	
+	# Constructor
+	def __init__(self):
+		self.past_actions = []
+	
+	# Appends a given action (assumed to be a single char)
+	def addPastAction(self, action_char):
+		self.past_actions.append(action_char)
+
+	# Returns the last (successful) action conducted by the agent
+	def getLatestAction(self):
+		return self.past_actions[-1]
+		
+	
