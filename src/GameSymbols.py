acc3851280b6255f997bf8003699cbec1403a821
Ian Wong <wong.iancw@gmail.com>
2016-05-10 20:23:10 +1000

    Added game symbol constants file

---
+++ src/GameSymbols.py
@@ -0,0 +1,21 @@
+# This file contains a list of all the symbols and characters used for the game
+# Namely actions, and tile characters
+
+# Tiles
+TILE_WALL = '*'
+TILE_WATER = '~'
+TILE_DOOR = '-'
+TILE_TREE = 'T'
+TILE_BLANK = ' '
+TILE_AXE = 'a'
+TILE_KEY = 'k'
+TILE_STEPPING_STONE = 'o'
+TILE_GOLD = 'g'
+TILE_START_POS = 's'
+
+# Constants
+ACTION_FORWARD = 'f'
+ACTION_RIGHT = 'r'
+ACTION_LEFT = 'l'
+ACTION_CHOP = 'c'
+ACTION_UNLOCK = 'u'
